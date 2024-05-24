544c9ea9f2c14cee30a9692369e142c2dce216b9
Julidmz <104100843+Julidmz@users.noreply.github.com>
2024-05-23 22:00:45 -0300

    Delete ProyectoDjango/Catalogo directory

--- ProyectoDjango/Catalogo/Productos/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm #Modelo del formulario por defecto de django
-from . import models               #Modelos de la aplicacion --> contiene las clases que vamos a importar
-
-class FormProductos(ModelForm):
-    class Meta:
-        model = models.Producto    # Importa el modelo Producto
-        fields =  '__all__'        # Este fields es para indicar que cuando renderice los campos, haga todos los campos que         tengan el modelo Producto que es el que importamos.
-                                   # Si quiero q no sean todos , especifico ese dato mediante una lista de string 
